bc0f15f85e6b16cfa17d719ac522c6ada9221d89
Marcotty <47335727+Marcotty@users.noreply.github.com>
2019-11-11 17:33:40 +0100

    Add message
    Labo2 modification

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world from HEYE Alan");
+            Console.WriteLine("Hello world from Marcotty Fred");
             Console.ReadLine();
         }
     }
